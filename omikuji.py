e0385505c29c56d70bd23133e1f4a463ff2e5679
kougyokuakihito100605-create <kougyokuakihito100605@gmail.com>
2026-06-21 05:08:35 +0900

    add omikuji.py

--- omikuji.py
+++ omikuji.py
@@ -1,15 +1,29 @@
-#運勢をリスト化する
-unsei=["大吉", "中吉", "小吉", "吉", "凶"]
-
-#それぞれの運勢に重みを付ける（合計100だとわかりやすい）
-#大吉40％、中吉25％、小吉20％、吉10％、凶5％
-weights=[40,25,20,10,5]
-
-#運勢をランダムに選ぶためにrandomモジュールをインポートする
+#おみくじアプリ
+import streamlit as st
 import random
 
-#randomモジュールのchoices関数を使って、運勢をランダムに選ぶ
-result=random.choices(unsei,weights=weights,k=1)[0]
+# タイトルを表示
+st.title("⛩️ デジタルおみくじ")
 
-#選んだ運勢を表示する
-print("今日の運勢は", result, "です。")
+#ボタンを作成
+if st.button('おみくじを引く'):
+    #運勢をリスト化する
+    unsei=["大吉", "中吉", "小吉", "吉", "凶"]
+
+    #それぞれの運勢に重みを付ける（合計100だとわかりやすい）
+    #大吉40％、中吉25％、小吉20％、吉10％、凶5％
+    weights=[40,25,20,10,5]
+
+    #randomモジュールのchoices関数を使って、運勢をランダムに選ぶ
+    result=random.choices(unsei,weights=weights,k=1)[0]
+
+    #選んだ運勢を表示する
+    st.header(f"今日の運勢は... 「{result}」です！")
+
+    #大吉の時だけの演出
+    if result=="大吉":
+       st.balloons()
+       st.success("おめでとうございます！、今日もよい一日になりますように")
+       
+#http://localhost:8501
+#http://172.31.25.141:8501
